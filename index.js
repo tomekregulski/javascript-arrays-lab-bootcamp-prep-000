8afaa249d8ed1257a4b251d74b6fd81694ef5fa9
Thomas Regulski <tomek.regulski@gmail.com>
2020-12-11 14:16:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ function destructivelyAppendKitten(name) {
   return kittens.push(name);
 }
 
+var kittens = ['Milo', 'Otis', 'Garfield']
+
 function destructivelyPrependKitten(name) {
   return kittens.unshift(name);
 }
