becf7d92b0ff8701ae2d7e6d6dafa99ee13e815c
Thomas Regulski <tomek.regulski@gmail.com>
2020-12-11 14:15:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 function destructivelyAppendKitten(name) {
   return kittens.push(name);
 }
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
+}
